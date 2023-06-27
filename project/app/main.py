e76cb94fc07f9b0406f147b47967894ddf360b33
SergeAvgust <avg89537878078@gmail.com>
2023-06-27 16:03:35 +0300

    Add database engine

--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
+from app.db import init_db
+from app.models import Song
+
 app = FastAPI()
 
+
+@app.on_event('startup')
+def on_startup():
+    init_db()
 
 @app.get("/ping")
 async def pong():
